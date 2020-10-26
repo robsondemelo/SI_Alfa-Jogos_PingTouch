275b9cc4db77e70136ff48344caecb0f32f388d7
Robson Melo <melodisouza@gmail.com>
2020-10-26 14:56:03 -0300

    Projeto Construct Robson_aulas Renan 1bim2020

---
+++ Exportado/PingTouchDoMalCompleto/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1586093575,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/parede-sheet0.png",
+		"images/blocos-sheet0.png",
+		"images/blocos-sheet1.png",
+		"images/plataforma-sheet0.png",
+		"images/bola-sheet0.png",
+		"images/particles.png",
+		"images/redball-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
